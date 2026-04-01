e16510c47f5018e7f4f1b4a47b7759050d49c1d7
Manasseh <103188466+Mbonimana@users.noreply.github.com>
2026-04-01 21:37:28 +0200

    Update base URL for production environment

--- src/config/baseUrl.ts
+++ src/config/baseUrl.ts
@@ -2,10 +2,10 @@ export const getBaseUrl = () => {
   // Check if running in the browser and on localhost
   if (typeof window !== 'undefined') {
     if (window.location.hostname === 'localhost') {
-      return 'http://localhost:3000/api';
+      return 'https://umubaji-hub.andasy.dev/api';
     }
   }
 
   // If not localhost, 
-  return 'http://localhost:3000/api'; // replace with your real deployed URL
+  return 'https://umubaji-hub.andasy.dev/api'; // replace with your real deployed URL
 };
