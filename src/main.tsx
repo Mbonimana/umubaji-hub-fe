a93d3f42caf51bb6094641fb74e394468870a247
Manasseh <103188466+Mbonimana@users.noreply.github.com>
2026-04-09 13:01:19 +0200

    Add HelmetProvider to main.tsx for SEO support

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,25 @@
-
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-
 import AuthProvider from './contexts/AuthProvider';     
 import CartProvider from './contexts/CartProvider';      
 import { WishlistProvider } from './contexts/WishlistContext';
 import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';  // ✅ add this
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <WishlistProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </WishlistProvider>
-      </AuthProvider>
-    </BrowserRouter>
+    <HelmetProvider> {/* ✅ wrap your app */}
+      <BrowserRouter>
+        <AuthProvider>
+          <WishlistProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </WishlistProvider>
+        </AuthProvider>
+      </BrowserRouter>
+    </HelmetProvider>
   </StrictMode>
 );
